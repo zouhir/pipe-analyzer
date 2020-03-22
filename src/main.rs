6b37768c10dd1538ea23107568190b3b8d83fcc8
Zouhir <zouhir@zouhir.org>
2020-03-22 11:01:14 -0700

    basic progress with precommit hooks

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,26 @@
+use std::env;
+use std::io::{self, Read, Write};
+
+const CHUNK_SIZE:
+usize = 16 * 1024; //16kb chunk size
+
 fn main() {
-    println!("Hello, world!");
+    let silent = !env::var("PV_SILENT").unwrap_or_default().is_empty();
+    //dbg!(silent);
+    let mut total_bytes = 0;
+    loop {
+        let mut buffer = [0; CHUNK_SIZE];
+
+        let num_read = match io::stdin().read(&mut buffer) {
+            Ok(0) => break,
+            Ok(x) => x,
+            Err(_) => break,
+        };
+        total_bytes += num_read;
+        //write all accepts a slice of bytes
+        io::stdout().write_all(&buffer[..num_read]).unwrap();
+    }
+    if !silent {
+        eprintln!("num_read: {}", total_bytes);
+    }
 }
